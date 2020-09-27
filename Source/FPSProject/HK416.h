0239cfa37a0ae45cf5d7c7383f9aafcec25080e9
Lukas Hüglin <lukas.hueglin@outlook.com>
2020-09-27 21:16:50 +0200

    22
    folders, Firearm and HK416 class added

---
+++ Source/FPSProject/HK416.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Firearm.h"
+#include "HK416.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSPROJECT_API AHK416 : public AFirearm
+{
+	GENERATED_BODY()
+	
+};
